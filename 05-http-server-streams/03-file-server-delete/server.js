b0dc47fdb05ccbfc23a2d997e5955f74d1be3634
Grigoriy Levanov <gregory@levanov.io>
2021-05-26 23:13:05 +0300

    удаление файла

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,22 +1,42 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+  try {
+    const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'DELETE':
+    switch (req.method) {
+      case 'DELETE':
+        const pathnameLength = pathname.split(path.sep).length;
+        if (pathnameLength > 1) {
+          res.statusCode = 400;
+          res.end();
+          break;
+        }
+        fs.access(filepath, fs.F_OK, (err) => {
+          if (err) {
+            res.statusCode = 404;
+            res.end();
+            return;
+          }
+          fs.unlink(filepath, () => res.end());
+        });
+        break;
 
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
+  } catch (e) {
+    console.error(e);
+    res.statusCode = 500;
+    res.end();
   }
 });
 
